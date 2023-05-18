411e9dab65e73ce6ee54ea6912951b3ac64231e7
Bouchra37 <ennahhalbouchra@gmail.com>
2023-05-18 18:57:06 +0100

    okey

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Header from "./components/Header"
 import Home from "./components/Home"
-import Footer from "./components/Footer"
+// import Footer from "./components/Footer"
 
 import "./App.css"
 
@@ -10,7 +10,7 @@ const App = () => {
     <>
       <Header />
       <Home />
-      <Footer />
+      {/* <Footer /> */}
     </>
   )
 }
